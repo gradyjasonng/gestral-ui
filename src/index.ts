98e9de6dd8b66d8a1d3c08aa15fa4c3190beec11
Grady Ng <gradyjasonng@gmail.com>
2026-07-02 14:11:49 +0800

    chore: export new primitives and components from index
    Wires up the public entry point for everything added in the previous
    commits: primitives (Text, Button, LabelledIconButton, Chip, Stack, Rail,
    Icon), new components (ArtboardWrapper, Card, FilterStrip, FrameLabel,
    PageRail, RailHeader, SiteRail, Prose, Sidebar), and the cn() utility.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
 import "./styles/tokens.css";
 import "./styles/global.css";
 
+// Utilities
+export { cn } from './lib/cn';
+
+// Primitives
+export { Text } from './primitives/Text/Text';
+export type { TextProps, TextVariant } from './primitives/Text/Text';
+export { Button } from './primitives/Button/Button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './primitives/Button/Button';
+export { LabelledIconButton } from './primitives/Button/LabelledIconButton';
+export type { LabelledIconButtonProps, LabelledIconButtonDirection } from './primitives/Button/LabelledIconButton';
+export { Chip } from './primitives/Chip/Chip';
+export type { ChipProps, ChipSize } from './primitives/Chip/Chip';
+export { Stack } from './primitives/Stack/Stack';
+export type { StackProps, StackDirection, StackAlign, StackJustify, StackGap, StackPadding } from './primitives/Stack/Stack';
+export { Rail } from './primitives/Rail/Rail';
+export type { RailProps } from './primitives/Rail/Rail';
+export { Icon } from './primitives/Icon/Icon';
+export type { IconProps, IconName, IconSize } from './primitives/Icon/Icon';
+
+// Components
+export { ArtboardWrapper } from './components/ArtboardWrapper/ArtboardWrapper';
+export type { ArtboardWrapperProps } from './components/ArtboardWrapper/ArtboardWrapper';
+export { Card } from './components/Card/Card';
+export type { CardProps, CardCategory } from './components/Card/Card';
+export { FilterStrip } from './components/FilterStrip/FilterStrip';
+export type { FilterStripProps, FilterOption } from './components/FilterStrip/FilterStrip';
+export { FrameLabel } from './components/FrameLabel/FrameLabel';
+export type { FrameLabelProps, FrameLabelSize } from './components/FrameLabel/FrameLabel';
+export { PageRail } from './components/PageRail/PageRail';
+export type { PageRailProps, TocItem, MetaEntry, PageItem, NodeIndent } from './components/PageRail/PageRail';
+export { RailHeader } from './components/RailHeader/RailHeader';
+export type { RailHeaderProps } from './components/RailHeader/RailHeader';
+export { SiteRail } from './components/SiteRail/SiteRail';
+export type { SiteRailProps, SiteRailItem, SiteRailFooterItem } from './components/SiteRail/SiteRail';
 export { Link } from './components/Link/Link';
 export type { LinkProps, LinkVariant } from './components/Link/Link';
-export { Mat } from './components/Mat/Mat';
-export type { MatProps } from './components/Mat/Mat';
-export { Text } from './components/Text/Text';
-export type { TextProps, TextVariant } from './components/Text/Text';
+export { Prose } from './components/Prose/Prose';
+export type { ProseProps } from './components/Prose/Prose';
+export { Sidebar } from './components/Sidebar/Sidebar';
+export type { SidebarProps } from './components/Sidebar/Sidebar';
